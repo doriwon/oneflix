2e8a047d4474a791d1e0ec67761b7eae5c8f6bbc
unknown <wldnjs7204@naver.com>
2021-05-14 00:47:05 +0900

    ~ #3.4 Location Aware Header

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import Header from "Components/Header";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
 
 export default () => (
     <Router>
-        <Switch> {/* 한 번에 오직 하나의 Route만 Render */}
-            <Route path="/" exact component={Home} />
-            <Route path="/tv" exact component={TV} /> {/* 중복되는 것 모두 노출이 되기 떄문에 exact를 넣어 그것만 노출되도록 */}
-            <Route path="/tv/hot" render={() => <h1>Hot</h1>} />  {/* 하나의 Route에 두 개의 Router를 Render할 때 사용 */}
-            <Route path="/search" component={Search} />
-            <Redirect from="*" to="/" />  {/* 일치하는 Route가 없으면 / (Home)로 이동 */}
-        </Switch>
+        <>
+            <Header />
+            <Switch> {/* 한 번에 오직 하나의 Route만 Render */}
+                <Route path="/" exact component={Home} />
+                <Route path="/tv" exact component={TV} /> {/* 중복되는 것 모두 노출이 되기 떄문에 exact를 넣어 그것만 노출되도록 */}
+                <Route path="/search" component={Search} />
+                <Redirect from="*" to="/" />  {/* 일치하는 Route가 없으면 / (Home)로 이동 */}
+            </Switch>
+        </>
     </Router>
 );
